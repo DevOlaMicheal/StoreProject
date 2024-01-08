23c293abd6e6a2d94bbbfdf52128b66505f18100
Micheal Ojedeji <97312093+DevOlaMicheal@users.noreply.github.com>
2024-01-08 23:51:03 +0100

    Added mongoose sorting, select, pagination and did a little regex

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -6,7 +6,7 @@ import asyncHandler from 'express-async-handler'
 
 const getProducts = async (req, res, next) => {
 
-    const {category, search} = req.query
+    const {category, search, sort, fields, page, limit, numericFilters} = req.query
 
     const qObject = {}
 
@@ -15,12 +15,61 @@ const getProducts = async (req, res, next) => {
     }
 
     if(search) {
-        qObject.productName = { $regex: search, $options: 'i' }
+        qObject.productName = { $regex: search, $options: 'i'}
     }
 
+    if(numericFilters) {
+
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        }
+
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+
+        const options = ['productPrice', 'quantity']
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-')
+            console.log(field, operator)
+
+            if(options.includes(field)) {
+                qObject[field] = {[operator]: Number(value)}
+            }
+        })
+
+        console.log(qObject)
+    }
+
+    console.log(qObject)
+
+
     try {
-        const product = await Product.find(qObject)
-        res.status(200).json({product})
+        let result = Product.find(qObject)
+        if(sort) {
+            const sortList = sort.split(',').join(' ');
+            result = result.sort(sortList)
+        }else{
+            result = result.sort('createdAt')
+        }
+        
+        if(fields){
+            const fieldList = fields.split(',').join(' ')
+            result = result.select(fieldList)
+        }
+
+        const pageNumber = Number(page) || 1
+        const limitNumber = Number(limit) || 5
+
+        const skip = (pageNumber -1) * limitNumber
+        result = result.skip(skip).limit(limitNumber)
+
+        const product = await result
+        res.status(200).json({number: product.length, product})
     } catch (error) {
         next(error)
     }
